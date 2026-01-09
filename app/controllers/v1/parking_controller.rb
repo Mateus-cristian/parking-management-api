c886a4209d66897d38d21752d24292df3be203d8
mateus cristian <mateus_cristian@outlook.com>
2026-01-09 19:12:16 -0300

    feat: update controllers for new repository/service interfaces

--- app/controllers/v1/parking_controller.rb
+++ app/controllers/v1/parking_controller.rb
@@ -1,73 +1,64 @@
 # frozen_string_literal: true
 
+require_relative '../../handlers/error_handlers'
+
 module V1
-  module ParkingController
-    def self.registered(app)
-      register_post_parking(app)
-      register_get_parking_history(app)
-      put_parking_checkout(app)
-      put_parking_payment(app)
+  class ParkingController < Sinatra::Base
+    register ErrorHandlers
+    set :show_exceptions, false
+
+    post '/v1/parking' do
+      body = JSON.parse(request.body.read)
+      idempotency_key = request.env['HTTP_IDEMPOTENCY_KEY']
+
+      parking_data = Services::ParkingEntryService.new.call(
+        plate: body['plate'],
+        idempotency_key: idempotency_key
+      )
+
+      status 201
+      { id: parking_data[:id] }.to_json
     end
 
-    def self.register_post_parking(app)
-      app.post '/v1/parking' do
-        body = JSON.parse(request.body.read)
-        idempotency_key = request.env['HTTP_IDEMPOTENCY_KEY']
+    get '/v1/parking/:plate' do
+      raw_plate = params['plate'].to_s.upcase.delete('-').strip
+      formatted_plate = raw_plate.insert(3, '-')
 
-        parking_data = Services::ParkingEntryService.new.call(
-          plate: body['plate'],
-          idempotency_key: idempotency_key
-        )
+      repo = Repositories::ParkingRepository.new
+      history = repo.find_history_by_plate(formatted_plate)
 
-        status 201
-        { id: parking_data[:id] }.to_json
-      end
-    end
+      if history.empty?
+        plate_exists = repo.plate_exists?(formatted_plate)
+        raise Errors::NotFoundError unless plate_exists
 
-    def self.register_get_parking_history(app)
-      app.get '/v1/parking/:plate' do
-        raw_plate = params['plate'].to_s.upcase.delete('-').strip
-        formatted_plate = raw_plate.insert(3, '-')
-
-        repo = Repositories::ParkingRepository.new
-        history = repo.find_history_by_plate(formatted_plate)
-
-        if history.empty?
-          plate_exists = repo.plate_exists?(formatted_plate)
-          raise Errors::NotFoundError unless plate_exists
-
-          content_type :json
-          return JSON.pretty_generate([])
-
-        end
         content_type :json
-        JSON.pretty_generate(history.map(&:to_hash))
+        return JSON.pretty_generate([])
+
       end
+      content_type :json
+      JSON.pretty_generate(history.map(&:to_hash))
     end
 
-    def self.put_parking_checkout(app)
-      app.put '/v1/parking/:id/out' do
-        idempotency_key = request.env['HTTP_IDEMPOTENCY_KEY']
+    put '/v1/parking/:id/out' do
+      idempotency_key = request.env['HTTP_IDEMPOTENCY_KEY']
 
-        Services::ParkingCheckoutService.new.call(
-          id: params['id'],
-          idempotency_key: idempotency_key
-        )
-        status 204
-      end
+      Services::ParkingCheckoutService.new.call(
+        id: params['id'],
+        idempotency_key: idempotency_key
+      )
+
+      status 204
     end
 
-    def self.put_parking_payment(app)
-      app.put '/v1/parking/:id/pay' do
-        idempotency_key = request.env['HTTP_IDEMPOTENCY_KEY']
+    put '/v1/parking/:id/pay' do
+      idempotency_key = request.env['HTTP_IDEMPOTENCY_KEY']
 
-        Services::ParkingPaymentService.new.call(
-          id: params['id'],
-          idempotency_key: idempotency_key
-        )
+      Services::ParkingPaymentService.new.call(
+        id: params['id'],
+        idempotency_key: idempotency_key
+      )
 
-        status 204
-      end
+      status 204
     end
   end
 end
